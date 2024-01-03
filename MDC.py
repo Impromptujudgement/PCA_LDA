48ba7123c62a80debfc3100ce0885d3d9cb8b970
Impromptujudgement <yunhuang23@m.fudan.edu.cn>
2024-01-03 16:59:07 +0800

    delete commits

--- MDC.py
+++ MDC.py
@@ -4,10 +4,6 @@
 from util import *
 
 
-def euclidean_distance(a, b):
-    return np.linalg.norm(a - b)
-
-
 def MDC(train_images, train_labels, test_images, test_labels):
     predicted_MDC = calculate_predicted_labels(train_images, train_labels, test_images)
     visualise_results(test_labels, predicted_MDC)
